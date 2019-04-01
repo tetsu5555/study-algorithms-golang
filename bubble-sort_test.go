ba837eff0a13deceba2b27587422efcdf07c7649
tetsu5555 <tetete1118@gmail.com>
2019-04-01 23:04:04 +0900

    add bubble-sort testing

---
+++ bubble-sort_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCalculate(t *testing.T) {
+	var toBeSorted [10]int = [10]int{1, 3, 2, 4, 8, 6, 7, 2, 3, 0}
+	sorted2 := BubbleSort(toBeSorted)
+	if sorted2 != [10]int{0, 1, 2, 2, 3, 3, 4, 6, 7, 8} {
+		t.Error("Expected to be Sorted")
+	}
+}
